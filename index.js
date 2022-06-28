b38b3e0fefca5904451cb0e171c73588c50e8fdf
Wonjong Park <wonjong2@gmail.com>
2022-06-27 23:15:16 -0700

    Changes DB name in connection.js file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, () => {
